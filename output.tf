fcc077b3e059bc578babe2f030d33b00124f94a6
Aleksei Sviridkin <f@lex.la>
2025-02-07 03:20:39 +0300

    Update output references to use cloudflare_dns_record instead of cloudflare_record
    Signed-off-by: Aleksei Sviridkin <f@lex.la>

--- output.tf
+++ output.tf
@@ -1,24 +1,24 @@
 output "mx_record_01_id" {
   description = "The ID of the first MX record for iCloud Mail"
-  value       = cloudflare_record.icloud_mx01.id
+  value       = cloudflare_dns_record.icloud_mx01.id
 }
 
 output "mx_record_02_id" {
   description = "The ID of the second MX record for iCloud Mail"
-  value       = cloudflare_record.icloud_mx02.id
+  value       = cloudflare_dns_record.icloud_mx02.id
 }
 
 output "signin_record_id" {
   description = "The ID of the CNAME record for iCloud Mail domain key (signin)"
-  value       = cloudflare_record.icloud_signin.id
+  value       = cloudflare_dns_record.icloud_signin.id
 }
 
 output "spf_txt_record_id" {
   description = "The ID of the TXT record for SPF settings"
-  value       = cloudflare_record.icloud_txt01.id
+  value       = cloudflare_dns_record.icloud_txt01.id
 }
 
 output "apple_domain_txt_record_id" {
   description = "The ID of the TXT record for apple-domain verification"
-  value       = cloudflare_record.icloud_txt02.id
+  value       = cloudflare_dns_record.icloud_txt02.id
 }
